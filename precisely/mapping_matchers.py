a69aa4a410414d1be45ad01e5fb82dc7c9a502b7
Erwin Junge <erwin@junge.nl>
2019-07-17 16:26:05 +0200

    Add mapping_includes matcher

--- precisely/mapping_matchers.py
+++ precisely/mapping_matchers.py
@@ -11,8 +11,9 @@ def is_mapping(matchers):
 
 
 class IsMappingMatcher(Matcher):
-    def __init__(self, matchers):
+    def __init__(self, matchers, allow_extra=False):
         self._matchers = matchers
+        self._allow_extra = allow_extra
 
     def match(self, actual):
         undefined = object()
@@ -25,14 +26,33 @@ class IsMappingMatcher(Matcher):
             if not value_result.is_match:
                 return unmatched("value for key {0!r} mismatched:{1}".format(key, indented_list([value_result.explanation])))
 
-        extra_keys = set(actual.keys()) - set(self._matchers.keys())
-        if extra_keys:
-            return unmatched("had extra keys:{0}".format(indented_list(sorted(map(repr, extra_keys)))))
+        if not self._allow_extra:
+            extra_keys = set(actual.keys()) - set(self._matchers.keys())
+            if extra_keys:
+                return unmatched("had extra keys:{0}".format(indented_list(sorted(map(repr, extra_keys)))))
 
         return matched()
 
     def describe(self):
         return "mapping with items:{0}".format(indented_list(sorted(
+            "{0!r}: {1}".format(key, matcher.describe())
+            for key, matcher in self._matchers.items()
+        )))
+
+
+def mapping_includes(matchers):
+    return MappingIncludesMatcher(dict(
+        (key, to_matcher(matcher))
+        for key, matcher in matchers.items()
+    ))
+
+
+class MappingIncludesMatcher(IsMappingMatcher):
+    def __init__(self, matchers):
+        super(MappingIncludesMatcher, self).__init__(matchers, allow_extra=True)
+
+    def describe(self):
+        return "mapping including items:{0}".format(indented_list(sorted(
             "{0!r}: {1}".format(key, matcher.describe())
             for key, matcher in self._matchers.items()
         )))
